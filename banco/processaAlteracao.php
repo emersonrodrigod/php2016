012bc1c9491d5f1915778a221b45dfb635a0efb6
Emerson Dario <emersonrodrigod@gmail.com>
2016-09-03 11:18:02 -0300

    alteracao

---
+++ banco/processaAlteracao.php
@@ -0,0 +1,25 @@
+<?php
+
+//Conecta com o banco de dados
+$conexao = new mysqli('localhost', 'root', '', 'emerson');
+
+//recupera as variaveis digitadas pelo usuario
+$nome = $_POST['nome'];
+$preco = $_POST['preco'];
+$id = $_POST['id'];
+
+//define o comando sql para inserir no banco
+$sql = "update produtos set nome = '$nome', preco = $preco where id = $id";
+
+//executa a inserção
+$query = $conexao->query($sql);
+
+//testa se deu certo
+if($query) {
+    header('location:index.php');
+}else {
+    echo "Aconteceu algum erro";
+}
+
+
+
